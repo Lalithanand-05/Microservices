46684fb300f00f6d17f3e043d28cd68e91c2b375
Lalithanand05 <46436522+Lalithanand-05@users.noreply.github.com>
2022-08-02 17:45:16 +0530

    Add files via upload

---
+++ Day 2/Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class DemoController : ApiController
+    {
+        static List<string> myList = new List<string>() { "value1", "value2", "value3" };
+        public IEnumerable<string> Get()
+        {
+            return myList;
+        }
+
+        public string Get(int id)
+        {
+            return myList[id];
+        }
+
+        public IEnumerable<string> Post([FromBody] string newval)
+        {
+            myList.Add(newval);
+            return myList;
+        }
+    }
+}
---
+++ Day 2/Models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+
+    }
+
+
+}
