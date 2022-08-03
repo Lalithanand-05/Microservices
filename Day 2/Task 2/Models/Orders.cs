5b0103608182f8a65234dcc90d37df4e65330f4c
Lalithanand05 <46436522+Lalithanand-05@users.noreply.github.com>
2022-08-03 13:50:10 +0530

    Add files via upload

---
+++ Day 2/Task 2/Models/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    
+        public class Orders
+        {
+            public int OrderId { get; set; }
+            public string CustomerName { get; set; }
+            public System.DateTime OrderDate { get; set; }
+            public double OrderAmount { get; set; }
+        }
+    }
